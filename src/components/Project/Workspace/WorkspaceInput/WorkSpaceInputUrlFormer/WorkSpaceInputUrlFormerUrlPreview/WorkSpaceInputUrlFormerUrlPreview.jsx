12648d28de2361d3e3e23d3afe805c47ef5e73ef
gordonchis1 <eduardovaldse@gmail.com>
2025-05-22 18:54:55 -0600

    refactor: change url preview to own component

---
+++ src/components/Project/Workspace/WorkspaceInput/WorkSpaceInputUrlFormer/WorkSpaceInputUrlFormerUrlPreview/WorkSpaceInputUrlFormerUrlPreview.jsx
@@ -0,0 +1,51 @@
+import { useState } from "react";
+import { faCheck, faCopy } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import useWorkSpaceContentContext from "../../../../../../Hooks/WorkSpace/useWorkSpaceContentContext";
+import "./WorkSpaceInputUrlFormerUrlPreview.css";
+
+export default function WorkSpaceInputUrlFormerUrlPreview() {
+  const [isCopied, setIsCopied] = useState(false);
+  const [content] = useWorkSpaceContentContext();
+
+  const handleCopyUrlPreview = async () => {
+    try {
+      await navigator.clipboard.writeText(content.url);
+      setIsCopied(true);
+      setTimeout(() => {
+        setIsCopied(false);
+      }, 700);
+    } catch {
+      setIsCopied(false);
+      throw new Error("Error on write clipboard");
+    }
+  };
+
+  return (
+    <div className="workspace-input-url-former_preview-url-container">
+      <div className="preview-url-title-container">
+        URL vista previa
+        <button
+          className="preview-url_copy-button"
+          onClick={handleCopyUrlPreview}
+        >
+          {isCopied ? (
+            <FontAwesomeIcon
+              icon={faCheck}
+              style={{ opacity: isCopied ? "1" : "0" }}
+              className="preview-url_copy-button-icon"
+              color="#22ff00"
+            />
+          ) : (
+            <FontAwesomeIcon
+              icon={faCopy}
+              style={{ opacity: isCopied ? "0" : "1" }}
+              className="preview-url_copy-button-icon"
+            />
+          )}
+        </button>
+      </div>
+      <div className="preview-url_preview-url-container">{content.url}</div>
+    </div>
+  );
+}
