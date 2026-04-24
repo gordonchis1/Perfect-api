6b234426b1f3c201861aa291141b1115cb13b95a
gordonchis1 <eduardovaldse@yahoo.com>
2026-04-23 21:52:59 -0600

    Add: delate all and delate single cookie

--- src/ui/components/Global/Modals/CookiesModal/CookiesHeaderControls/CookiesHeaderControls.jsx
+++ src/ui/components/Global/Modals/CookiesModal/CookiesHeaderControls/CookiesHeaderControls.jsx
@@ -1,11 +1,29 @@
+import { CookieJar } from "tough-cookie"
+import { useProjectStore } from "../../../../../stores/ProjectStore"
 import Button from "../../../Button/Button"
 import "./CookiesHeaderControls.css"
 import { Plus, Trash2 } from "lucide-react"
 
 export default function CookiesHeaderControls() {
+    const updateCookies = useProjectStore(store => store.updateCookies)
+    const handleDelateAll = () => {
+        const newJar = new CookieJar()
+        const jsonJar = newJar.toJSON()
+        updateCookies(jsonJar)
+    }
+    const handleAddCookie = () => {
+        // pass 
+    }
+
+
     return <div className="cookies-header-controls_container">
-        <Button text={"Add cookie"} icon={<Plus size={17} />} />
         <Button
+            text={"Add cookie"}
+            onClick={handleAddCookie}
+            icon={<Plus size={17} />}
+        />
+        <Button
+            onClick={handleDelateAll}
             text={"Delate All"}
             icon={<Trash2 size={17} />}
             className={"delate-btn"}
--- src/ui/components/Global/Modals/CookiesModal/CookiesTable/CookiesTable.jsx
+++ src/ui/components/Global/Modals/CookiesModal/CookiesTable/CookiesTable.jsx
@@ -1,14 +1,15 @@
-import { Cookie } from "tough-cookie"
+import { Cookie, CookieJar } from "tough-cookie"
 import { useProjectStore } from "../../../../../stores/ProjectStore"
 import "./CookiesTable.css"
 import { Pencil, Trash2 } from "lucide-react"
 
 export default function CookiesTable() {
-    const { cookies } = useProjectStore(store => store.cookies)
+    const updateCookies = useProjectStore(store => store.updateCookies)
+    const cookies = useProjectStore(store => store.cookies)
 
     const convertToCookieInstance = () => {
         const parsedCookies = []
-        for (let cookie of cookies) {
+        for (let cookie of cookies.cookies) {
             const parsedCookie = Cookie.fromJSON(cookie)
             parsedCookies.push(parsedCookie)
         }
@@ -16,13 +17,25 @@ export default function CookiesTable() {
     }
     const cookiesToRender = convertToCookieInstance()
 
+    const handleDelateCookie = (cookie, idx) => {
+        const updatedCookies = structuredClone(cookies)
+        const delatedCookie = updatedCookies.cookies.splice(idx, 1)
+
+        if (!delatedCookie) return
+
+        const delatedCookieJson = cookie.toJSON()
+        if (delatedCookie[0].creation == delatedCookieJson.creation && delatedCookie[0].key == delatedCookieJson.key) {
+            updateCookies(updatedCookies)
+        }
+    }
+
     return <div className="cookies-table_container">
         {cookies && cookiesToRender.map((cookie, idx) => {
             return <div className="cookie_container" style={{ background: idx % 2 == 0 ? "var(--primary-transparent)" : "" }}>
                 <p>{cookie.domain}</p>
                 <p>{cookie.toString()}</p>
                 <div className="cookie-btn_container">
-                    <button className="cookie-btn" ><Trash2 color="var(--destructive)" /></button>
+                    <button className="cookie-btn" onClick={() => handleDelateCookie(cookie, idx)}><Trash2 color="var(--destructive)" /></button>
                     <button className="cookie-btn"><Pencil /></button>
                 </div>
             </div>
